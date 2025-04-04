fb851badb590087a12a8b47b24383515bbf93b9a
Orca-03 <98732343+Orca-03@users.noreply.github.com>
2025-04-04 14:35:36 -0700

    Java and XML files added

--- app/src/main/java/com/example/evergrowtesting/A_editTaskActivity.java
+++ app/src/main/java/com/example/evergrowtesting/A_editTaskActivity.java
@@ -8,33 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class A_editTaskActivity extends AppCompatActivity {
 
-    ArrayList<Task> dailyTasks = new ArrayList<>();
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+    @Override
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_task);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        setUpDailyTasks();
-    }
-
-    public void setUpDailyTasks() {
-        String[] taskNames = getResources().getStringArray(R.array.bogy_data);
-
-        for(int i=0; i<taskNames.length; i++) {
-            dailyTasks.add(new Task(taskNames[i]));
-        }
-
-
     }
 }
--- app/src/main/java/com/example/evergrowtesting/EditGoalActivity.java
+++ app/src/main/java/com/example/evergrowtesting/EditGoalActivity.java
@@ -8,33 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    ArrayList<Task> dailyTasks = new ArrayList<>();
+public class EditGoalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_goal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        setUpDailyTasks();
-    }
-
-    public void setUpDailyTasks() {
-        String[] taskNames = getResources().getStringArray(R.array.bogy_data);
-
-        for(int i=0; i<taskNames.length; i++) {
-            dailyTasks.add(new Task(taskNames[i]));
-        }
-
-
     }
 }

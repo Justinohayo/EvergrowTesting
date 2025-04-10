ed8feb7b44a2fded725535a7242e944ba6a20d4a
Justinohayo <159392063+Justinohayo@users.noreply.github.com>
2025-04-10 14:43:56 -0700

    update btn_save and id

--- app/src/main/java/com/example/evergrowtesting/A_editTaskActivity.java
+++ app/src/main/java/com/example/evergrowtesting/A_editTaskActivity.java
@@ -1,6 +1,7 @@
 package com.example.evergrowtesting;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class A_editTaskActivity extends AppCompatActivity {
 
-//    protected void onCreate(Bundle savedInstanceState) {
-//    @Override
-//        super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_edit_task);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//    }
+    Button btn_save;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_task);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 
 
 }
